8ddf24281a1fdffe24b7e6f4565cd82227077f32
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-25 11:23:29 -0400

    refactor: executing init commands

--- forge.py
+++ forge.py
@@ -2,9 +2,16 @@ import json
 import os
 
 filename = 'forgefile.json'
+initialize_command_key = '.init'
 
 with open(filename) as infile:
     contents = json.load(infile)
 
-for key, value in contents.items():
-    print('key', key, 'value', value)
+initialize_commands = contents.get(initialize_command_key, None)
+
+if initialize_commands:
+    for command in initialize_commands:
+        print('command', command)
+        # os.system(command)
+
+
