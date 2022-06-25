88794d7e524d8226f81a66421974188421f2e774
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-25 17:01:31 -0400

    refactor: removed print statement

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,5 @@ pub fn parse_forge_commands(filename: &str) -> serde_json::Value {
     let file = File::open(filename).unwrap();
     let json: serde_json::Value =
         serde_json::from_reader(file).unwrap();
-    println!("{:?}", json);
     return json;
 }
